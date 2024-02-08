876ebca89d6d7e18b6203cf4d4d7b273d250a7a1
Yusuf Mirza Altay <ymiirza@gmail.com>
2024-02-08 15:34:15 +0300

    image generation completed

--- frontend/src/lib/components/ImageMessages.tsx
+++ frontend/src/lib/components/ImageMessages.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { Card, CardBody } from "@nextui-org/card";
-import EmptyComponent from "./EmptyComponent";
 import { Image } from "@nextui-org/image";
+import EmptyComponent from "./EmptyComponent";
 
 const ImageMessages = ({ imageUrls }: { imageUrls: string[] }) => {
 	return imageUrls.length === 0 ? (
 		<EmptyComponent label='No images generated yet.' />
 	) : (
-		<div className=''>
+		<div className='md:flex gap-4 grid grid-cols-2'>
 			{imageUrls.map((image: any) => {
 				return (
-					<Card className='p-4' key={image.url}>
+					<Card className='p-0 lg:p-4' key={image.url}>
 						<CardBody>
 							<Image
 								src={image.url}
