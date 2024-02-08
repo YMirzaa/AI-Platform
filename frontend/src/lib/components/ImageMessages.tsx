b671229faf3da8374f4899c4c76bd11257b228f7
Yusuf Mirza Altay <ymiirza@gmail.com>
2024-02-08 15:53:28 +0300

    download added

--- frontend/src/lib/components/ImageMessages.tsx
+++ frontend/src/lib/components/ImageMessages.tsx
@@ -1,24 +1,38 @@
 "use client";
 
-import { Card, CardBody } from "@nextui-org/card";
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
 import { Image } from "@nextui-org/image";
+import { Button } from "@nextui-org/react";
+import { Download } from "lucide-react";
 import EmptyComponent from "./EmptyComponent";
 
 const ImageMessages = ({ imageUrls }: { imageUrls: string[] }) => {
 	return imageUrls.length === 0 ? (
 		<EmptyComponent label='No images generated yet.' />
 	) : (
-		<div className='md:flex gap-4 grid grid-cols-2'>
+		<div className='md:flex gap-4 grid grid-cols-2 '>
 			{imageUrls.map((image: any) => {
 				return (
-					<Card className='p-0 lg:p-4' key={image.url}>
-						<CardBody>
+					<Card className='p-0 lg:p-4 ' key={image.url}>
+						<CardBody className='flex items-center'>
 							<Image
 								src={image.url}
 								alt='Generated Image'
 								width={240}
 							/>
 						</CardBody>
+						<CardFooter>
+							<Button
+								color='secondary'
+								className='w-full'
+								onClick={() => {
+									window.open(image.url, "_blank");
+								}}
+							>
+								<Download size={16} />
+								Download
+							</Button>
+						</CardFooter>
 					</Card>
 				);
 			})}
